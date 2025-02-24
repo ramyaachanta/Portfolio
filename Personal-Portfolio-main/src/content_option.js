e861b817b61f6549980a86fc8a3c17451f3a90ba
ramyaachanta <64644325+ramyaachanta@users.noreply.github.com>
2025-02-24 10:52:09 -0600

    email trigger 1

--- Personal-Portfolio-main/src/content_option.js
+++ Personal-Portfolio-main/src/content_option.js
@@ -183,6 +183,7 @@ const contactConfig = {
     YOUR_SERVICE_ID: "service_lcyxvyc",
     YOUR_TEMPLATE_ID: "template_3d7nqgu",
     YOUR_USER_ID: "Ryitj2rXLhwfaij0K",
+    
 };
 
 const socialprofils = {
